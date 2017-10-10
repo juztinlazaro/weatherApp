ef1733c5147423e639d8cb82ee6780a718ec8a0c
Justin Lazaro <juztinlazaro@gmail.com>
2017-10-10 18:08:59 +0800

    refractor reducer_weather

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,8 +1,8 @@
-import FETCH_WEATHER from '../actions';
+import { FETCH_WEATHER } from '../actions';
 
 export default function (state = [], action) {
 	switch (action.type) {
-		case 'FETCH_WEATHER':
+		case FETCH_WEATHER:
 		state = [ action.payload.data, ...state ];
 		return state;
 	}
